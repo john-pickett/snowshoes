cbd68d63279e6ba6ef9f6ee9d780fb741a35a81a
John Pickett <john.pickett@maryville.com>
2019-06-17 10:28:14 -0500

    added async foreach to update all

--- scripts/update_all.js
+++ scripts/update_all.js
@@ -1,12 +1,7 @@
 const fs = require('fs');
 const snowAxios = require('../config/axios.js');
 const registry = require('../config/registry.json');
-
-// const url = '/api/now/table/' + item_data.table + "/" + item_data.sys_id;
-//const folder = item_data.table;
-//const file_name = script_name + '.js';
-// let updated_script = fs.readFileSync(`./service-now/${folder}/${file_name}`, 'utf8');
-
+const { asyncForEach } = require('./utils.js');
 
 const putScript = (url, updated_script) => {
 	return new Promise((resolve, reject) => {
@@ -20,6 +15,7 @@ const putScript = (url, updated_script) => {
 
 const updateAllScripts = async (registry) => {
 	registry.forEach(async (script) => {
+		console.log('putting ' + script.table)
 		let url = '/api/now/table/' + script.table + "/" + script.sys_id;
 		let updated_script = fs.readFileSync(`./service-now/${script.table}/${script.name}.js`, 'utf8');
 		const result = await putScript(url, updated_script);
@@ -27,4 +23,12 @@ const updateAllScripts = async (registry) => {
 	})
 }
 
-updateAllScripts(registry);
+const start = (async () => {
+	await asyncForEach(registry, async (script) => {
+		console.log('putting ' + script.table)
+		let url = '/api/now/table/' + script.table + "/" + script.sys_id;
+		let updated_script = fs.readFileSync(`./service-now/${script.table}/${script.name}.js`, 'utf8');
+		const result = await putScript(url, updated_script);
+		console.log(result);
+	})
+})();
